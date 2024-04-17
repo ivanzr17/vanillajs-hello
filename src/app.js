4398be559c1e532dc116e7ab127d9687ce44876d
ivanzr17 <139049435+ivanzr17@users.noreply.github.com>
2024-04-17 16:21:21 +0000

    Primer Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const quien = ["Mi perro", "Mi abuelo", "Mi gato", "Mi pez"];
+const hizo = ["se comió", "vendió", "fumó", "destruyó"];
+const que = ["mi tarea", "mi casa", "mi telefono", "mi dinero"];
+const cuando = ["ayer", "hoy", "el año pasado", "la semana pasada"];
+
+function randomIndex(len) {
+  return Math.floor(Math.random() * len);
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hola Mundo!");
+
+  //Crear un excusa aleatoria
+
+  const excusa =
+    quien[randomIndex(quien.length)] +
+    " " +
+    hizo[randomIndex(hizo.length)] +
+    " " +
+    que[randomIndex(que.length)] +
+    " " +
+    cuando[randomIndex(cuando.length)];
+
+  // Modificar el HTML para que muestre exucusas aleatorias
+
+  document.querySelector("div").innerText = excusa;
 };
